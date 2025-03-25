4f791e667f0d2cbe5e44e81ec18914eb2155c333
Jefferson Silva <jeff.silvadsouza@gmail.com>
2025-03-25 15:45:29 -0300

    change conditional operator

--- src/native/conditions.rs
+++ src/native/conditions.rs
@@ -2,35 +2,36 @@ mod conditions {
     use crate::{aly::Aly, lexer::Lexer, native::{exec_rust, process_value, types::Validator}};
 
     pub fn exec_cond(run: &mut Aly, expressions: Vec<Lexer>) -> Box<dyn Validator> {
-        let find = Vec::new(
-            vec!["eq", "=="],
-            vec!["neq", "!="],
-            vec!["lt", "<"],
-            vec!["gt", ">"],
+        let find: Vec<Vec<&str>> = vec![
             vec!["lte", "<="],
             vec!["gte", ">="],
+            vec!["neq", "!="],
+            vec!["eq", "=="],
+            vec!["lt", "<"],
+            vec!["gt", ">"],
             vec!["and", "&&"],
             vec!["or", "||"],
             vec!["xor", "^"],
-        );
-        
+        ];
+
         let mut expr = vec![];
 
         for exp in expressions {
             if exp.token.id() == "reference" {
                 let item = process_value(run, [exp].to_vec());
-
                 expr.push(item.to_string(false));
             } else {
                 expr.push(exp.literal);
             }
         }
 
-        for item in expr {
-            println!("{}", item);
-        }
+        let mut expr_final = expr.join(""); 
 
-        let res = match exec_rust(expr.join(" ")) {
+        for item in find{
+            expr_final = expr_final.replace(item[0], item[1])
+        }
+        
+        let res = match exec_rust(expr_final) {
             Ok(boolean) => boolean,
             Err(_) => String::from("false"),
         };
